d932c46864b60cdc54cffdc30143aaeb36648c83
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-01 01:02:27 +0530

    Hh

--- Config.py
+++ Config.py
@@ -5,7 +5,7 @@ class Config():
   if ENV:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DATABASE_URL = os.environ.get("DATABASE_URL", None)
-    APP_ID = os.environ.get("APP_ID", 6)
+    API_ID = os.environ.get("API_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
     SUDO_USERS.append(749673781)
@@ -13,7 +13,7 @@ class Config():
   else:
     BOT_TOKEN = ""
     DATABASE_URL = ""
-    APP_ID = ""
+    API_ID = ""
     API_HASH = ""
     SUDO_USERS = list(set(int(x) for x in ''.split()))
     SUDO_USERS.append(749673781)
