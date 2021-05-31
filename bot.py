8653dcd0d1b9338a0e290feb501b130f83af0eb3
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-06-01 00:18:18 +0530

    H

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ app = Client(
       bot_token = Config.BOT_TOKEN,
       api_id = Config.APP_ID,
       api_hash = Config.API_HASH,
-      plugins = mwksub
+      plugins = {"mwk": "mwk"}
 )
 
 app.run()
